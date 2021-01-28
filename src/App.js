dd50e344e452188b00fad048203694dd7b578c49
hardy lutula <hardylutula@gmail.com>
2021-01-28 14:41:08 +0200

    Posts components added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-// https://jsonplaceholder.typicode.com/posts
+
 
 const App = () => {
   // holds posts
   const [posts, setPosts] = useState([]);
+
   // block ui while fetching data
   const [loading, setLoading] = useState(false);
+
   // pagination
   const [currentPage, setCurrentPage] = useState(1);
+
   // holds number of posts per page
   const [postsPerPage, setpostsPerPage] = useState(10);
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(data);
+      console.log('hardy');
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
 
   return (
     <div>
