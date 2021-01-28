226be8b7f551cb911b6fccff0ea29864b52a3784
hardy lutula <hardylutula@gmail.com>
2021-01-28 11:51:45 +0200

    initiated default values

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// https://jsonplaceholder.typicode.com/posts
 
 const App = () => {
+  // holds posts
+  const [posts, setPosts] = useState([]);
+  // block ui while fetching data
+  const [loading, setLoading] = useState(false);
+  // pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  // holds number of posts per page
+  const [postsPerPage, setpostsPerPage] = useState(10);
+
   return (
     <div>
       <h1>Pagination</h1>
